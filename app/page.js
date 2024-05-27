a4abc1ed1afaaf7fda249bd4ebb99514ae4f1b5c
noorverma <vermanoor89@gmail.com>
2024-05-27 11:24:47 -0600

    first commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/http://localhost:3000/week-2">
+      <Link href="/week-2">
         Go to Week 2 Assignment
       </Link>
     </main>
