cce47aa5919b071c5cbd07e4f5f9fd1ce00c2025
noorverma <vermanoor89@gmail.com>
2024-05-27 10:35:09 -0600

    Week2

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/http://localhost:3000/week-2">
+        Go to Week 2 Assignment
+      </Link>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Noor</p>
+      <Link href="https://github.com/noorverma/cprg306-assignments">
+        github
+      </Link>
+    </div>
+  );
+}
