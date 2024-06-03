615ee9f04011fee7cbdc0d2d2b77da982e98fae8
noorverma <vermanoor89@gmail.com>
2024-06-03 08:57:47 -0600

    first commit

--- app/page.js
+++ app/page.js
@@ -4,9 +4,14 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <div>
       <Link href="/week-2">
         Go to Week 2 Assignment
-      </Link>
+      </Link></div>
+      <div>
+      <Link href="/week-3">
+        Go to Week 3 Assignment
+        </Link></div>
     </main>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,10 +2,9 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <main>
+    <main >
       <h1>Shopping List</h1>
       <StudentInfo />
     </main>
   );
 }
-  
