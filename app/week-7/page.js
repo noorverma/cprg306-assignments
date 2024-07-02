86633e0a14d049f1fd2d614cc8c636ed5b762b56
noorverma <vermanoor89@gmail.com>
2024-07-02 09:12:32 -0600

    first commit

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,24 +1,28 @@
 'use client';
- 
-import React, { useState} from 'react';
+
+import React, { useState } from 'react';
 import NewItem from './new-item';
 import ItemList from './item-list';
 import MealIdeas from './meal-ideas';
 import itemsData from './items.json';
- 
+
 export default function Page() {
   const [items, setItems] = useState(itemsData);
   const [selectedItemName, setSelectedItemName] = useState('');
- 
+
   const handleAddItem = (newItem) => {
     setItems([...items, newItem]);
   };
- 
+
   const handleItemSelect = (itemName) => {
-    const cleanedItemName = itemName.split(' ')[0].replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+    const cleanedItemName = itemName
+      .split(',')[0] // Assuming the format is "spaghetti, 1 kg"
+      .replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
+      .trim();
+    console.log(`Selected item name cleaned: ${cleanedItemName}`); // Log the cleaned item name
     setSelectedItemName(cleanedItemName);
   };
-  
+
   return (
     <main className="bg-indigo-200 min-h-screen p-4 flex">
       <div className="flex-1">
